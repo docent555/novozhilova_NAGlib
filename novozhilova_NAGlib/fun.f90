f850e99bcec9d9cea1d23a5b481815320d7dd975
Alex <Alex@DESKTOP-3EFEPPB>
2022-06-24 22:44:51 +0300

    24.06.2022

--- novozhilova_NAGlib/fun.f90
+++ novozhilova_NAGlib/fun.f90
@@ -2,13 +2,20 @@ module fun
     use, intrinsic :: iso_c_binding
     use ifcore
 
-    integer(c_int) ne, nt, nz, freq_out
-    real(c_double) zex, dz, tend, dtr(2), q(3), i(2), th(2), a(2), dcir(2), r(2), f0(3), dt, pitch, f10, f20, f30
+    integer(c_int) ne, nt, nz, freq_out, neqp, lenwrk, l, method, ifail, neqf, lwork, liwork, nrd
+ real(c_double) zex, dz, tend, dtr(2), q(3), i(2), th(2), a(2), dcir(2), r(2), f0(3), dt, pitch, f10, f20, f30, ptol, hstart, zstart
     complex(c_double_complex) fp(2)
+    logical(c_bool) errass
+    
+    integer(c_int) breaknum(3)
+    real(c_double) phitmp0(3), phitmp1(3)
+    complex(c_double_complex) fc, fcarr(3)
 
-    integer(c_int), allocatable, target :: idxre(:, :), idxim(:, :)
+    integer(c_int), allocatable, target :: idxre(:, :), idxim(:, :), iwork(:)
     complex(c_double_complex), allocatable, target :: mean(:)
-    real(c_double), allocatable, target :: tax(:), zax(:), u(:), eta(:, :), etag(:, :), w(:, :), f(:, :), w1(:, :), p(:, :)
+    real(c_double), allocatable, target :: tax(:), zax(:), u(:), eta(:, :), etag(:, :), w(:, :), f(:, :), w1(:, :), p(:, :), &
+                                           phi(:, :), phios(:, :), wos(:, :), &
+                                           pgot(:), ppgot(:), pmax(:), thres(:), workp(:), work(:)
 
     complex(c_double_complex), parameter :: ic = (0.0d0, 1.0d0)
     real(c_double), parameter :: pi = 2.0D0*dacos(0.0D0)
@@ -27,7 +34,25 @@ contains
         nt = tend/dt + 1
         nz = zex/dz + 1
 
+        neqp = 4*ne
+        lenwrk = 32*neqp
+        zstart = 0.0d0
+        errass = .false.
+        hstart = 0.0d0
+        ptol = 1.0d-7
+        method = 2
+        ifail = 0
+
+        neqf = 6
+        nrd = 6
+        lwork = 8*neqf + 5*nrd + 21
+        liwork = nrd + 21
+
         call allocate_arrays()
+
+        do l = 1, neqp
+            thres(l) = 1.0d-8
+        end do
 
         f(1, 1) = f10
         f(3, 1) = f20
@@ -63,8 +88,9 @@ contains
 
         integer(c_int) err_alloc
 
-        allocate (f(6, nt), p(4*ne, nz), u(nz), tax(nt), zax(nz), mean(nz), eta(2, nt), etag(2, nt), w(3, nt - 1), w1(3, nt - 1), &
-                  idxre(2, ne), idxim(2, ne), stat=err_alloc)
+        allocate (f(6, nt), p(neqp, nz), u(nz), tax(nt), zax(nz), mean(nz), eta(2, nt), etag(2, nt), w(3, nt - 1), w1(3, nt - 1), &
+                  idxre(2, ne), idxim(2, ne), pgot(neqp), ppgot(neqp), pmax(neqp), thres(neqp), workp(lenwrk), work(lwork), iwork(liwork), &
+                  wos(3, nt - 1), phi(3, nt), phios(3, nt), stat=err_alloc)
 
         if (err_alloc /= 0) then
             print *, "allocation error"
@@ -125,83 +151,56 @@ contains
 102     print *, 'error of reading file "input_fortran.dat"'; pause; stop
     end subroutine read_param
 
-    subroutine ode4f(dydt, y, neq, nt, t0, h)
+    subroutine ode4f()
         import
         implicit none
 
-        interface
-            function dydt(t, y) result(s)
-                use, intrinsic :: iso_c_binding
-                implicit none
-                real(c_double) t, y(:), s(size(y))
-            end function dydt
-        end interface
-
-        integer(c_int) nt, neq, i, j
-        real(c_double) h, t0, t
-        real(c_double), intent(inout) :: y(:, :)
-        real(c_double) s1(size(y, 1)), s2(size(y, 1)), s3(size(y, 1)), s4(size(y, 1)), v(size(y, 1))
+        integer(c_int) nt, neq, i, j, ipar, iout, idid, itol
+        real(c_double) :: h, t, zwant, zgot, rpar, rtol, atol
+        real(c_double) y(6)
         logical(4) pressed
         character(1) key
         integer(c_int), parameter :: esc = 27
 
+        call d02pvf(neqp, zstart, p(:, 1), zex, ptol, thres, method, 'usual task', errass, hstart, workp, lenwrk, ifail)
+
         !solve eq. at t=0
-        fp(1) = y(1, 1)*exp(ic*y(2, 1))
-        fp(2) = y(3, 1)*exp(ic*y(4, 1))
-        call ode4p(dpdz, p, ne, nz, 0.0d0, dz)
-        !CALL D02BGF(0.0d0,zex,4*ne,Y,TOL,HMAX,M,VAL,FCN,W,IFAIL)
+        fp(1) = f(1, 1)*exp(ic*f(2, 1))
+        fp(2) = f(3, 1)*exp(ic*f(4, 1))
+
+        do i = 1, nz - 1
+            zwant = i*dz
+            call d02pcf(dpdz, zwant, zgot, pgot, ppgot, pmax, workp, ifail)
+            p(:, i + 1) = pgot
+        end do
+
         eta(:, 1) = eff(p(:, nz))
         etag(:, 1) = pitch**2/(pitch**2 + 1)*eta(:, 1)
 
-        do i = 1, nt - 1
-            v = y(:, i)
-            t = t0 + (i - 1)*h
-            s1(:) = dydt(t, v)
-            s2(:) = dydt(t + h/2, v + h*s1(:)/2)
-            s3(:) = dydt(t + h/2, v + h*s2(:)/2)
-            s4(:) = dydt(t + h, v + h*s3(:))
-            y(:, i + 1) = v + h*(s1(:) + 2*s2(:) + 2*s3(:) + s4(:))/6
+        !open (1, file='test.dat')
+        !do j = 1, nz
+        !    write (1, '(5f17.8)') (j-1)*dz, p(14, j), p(ne+14, j), p(128, j), p(ne+128, j)
+        !end do
+        !close (1)
+        !stop
 
-            eta(:, i + 1) = eff(p(:, nz))
-            etag(:, i + 1) = pitch**2/(pitch**2 + 1)*eta(:, i + 1)
+        rpar = 0.0
+        ipar = 0
+        t = tax(1)
+        y = f(:, 1)
+        itol = 0
+        rtol = 1.0d-7
+        atol = rtol
+        iout = 6
 
-            write (*, '(a,f12.7,a,f10.7,a,f10.7,a,f10.7,a,f10.7,a,f10.7,\,a)') 'Time = ', t + h, '   |F1| = ', abs(y(1, i + 1)), '   |F2| = ', abs(y(3, i + 1)), '   |F3| = ', abs(y(5, i + 1)), &
-                '   Eff1 = ', eta(1, i + 1), '   Eff2 = ', eta(2, i + 1), char(13)
+        iwork(:) = 0
+        work(:) = 0.0d0
 
-            pressed = peekcharqq()
-            if (pressed) then
-                key = getcharqq()
-                if (ichar(key) .eq. esc) then
-                    write (*, '(/,a)') 'Quit?'
-                    key = getcharqq()
-                    if (ichar(key) .eq. 121 .or. ichar(key) .eq. 89) then
-                        open (1, file='F.dat')
-                        do j = 1, i + 1
-                            write (1, '(4e17.8)') (j - 1)*h, abs(y(1, j)), abs(y(2, j)), abs(y(3, j))
-                        end do
-                        close (2)
-                        open (2, file='E.dat')
-                        do j = 1, i + 1
-                            write (2, '(5e17.8)') (j - 1)*h, eta(1, j), etag(1, j), eta(2, j), etag(2, j)
-                        end do
-                        close (2)
-                        stop
-                    end if
-                end if
-            end if
-        end do
+        iwork(5) = 6
+
+        call dopri5(6, dfdt, t, y, tend, rtol, atol, itol, solout, iout, work, lwork, iwork, liwork, rpar, ipar, idid)
+
     end subroutine ode4f
-
-    !function eff(pex, ne) result(eta)
-    !    import
-    !
-    !    integer(c_int) ne
-    !    real(c_double) eta(2)
-    !    complex(c_double_complex), intent(in) :: pex(:)
-    !
-    !    eta(1) = 1 - sum(abs(pex(1:ne))**2)/ne
-    !    eta(2) = 1 - sum(abs(pex(ne + 1:2*ne))**2)/ne
-    !end function eff
 
     function eff(pex) result(eta)
         use, intrinsic :: iso_c_binding, only: c_double, c_int
@@ -214,61 +213,15 @@ contains
         real(c_double), intent(in) :: pex(:)
 
         do i = 1, 2
-            eta(i) = 1 - sum(sqrt(pex(idxre(i, :))**2 + pex(idxim(i, :))**2))/ne
+            eta(i) = 1 - sum(abs(dcmplx(pex(idxre(i, :)), pex(idxim(i, :))))**2)/ne
         end do
     end function eff
-
-    subroutine ode4p(dydt, y, neq, nz, z0, h)!, params)
-        import
-        implicit none
-
-        interface
-            subroutine dydt(z, p, prhs)
-                use, intrinsic :: iso_c_binding
-                implicit none
-                real(c_double), intent(in) :: z, p(:)
-                real(c_double), intent(out) :: prhs(:)
-            end subroutine dydt
-        end interface
-
-        integer(c_int) nz, neq, i
-        real(c_double) h, z0, z
-        real(c_double), intent(inout) :: y(:, :)
-        real(c_double) s1(size(y, 1)), s2(size(y, 1)), s3(size(y, 1)), s4(size(y, 1)), v(size(y, 1))
-
-        do i = 1, nz - 1
-            v = y(:, i)
-            z = z0 + (i - 1)*h
-            call dydt(z, v, s1)
-            call dydt(z + h/2, v + h*s1(:)/2, s2)
-            call dydt(z + h/2, v + h*s2(:)/2, s3)
-            call dydt(z + h, v + h*s3(:), s4)
-            y(:, i + 1) = v + h*(s1(:) + 2*s2(:) + 2*s3(:) + s4(:))/6
-        end do
-    end subroutine ode4p
-
-    !function dpdz(z, p) result(s)
-    !    import
-    !    implicit none
-    !
-    !    integer(c_int) i, idx(ne)
-    !    real(c_double) u, z
-    !    complex(c_double_complex), parameter :: ic = (0.0D0, 1.0D0)
-    !    complex(c_double_complex) p(:), s(size(p, 1))
-    !
-    !    do i = 1, 2
-    !        u = exp(-3*((z - zex/2)/(zex/2))**2)
-    !        idx = (/(i - 1)*ne + 1:ne + (i - 1)*ne/)
-    !        s(idx) = ic*(fp(i)*u - (dtr(i) + abs(p(idx))**2 - 1)*p(idx))
-    !    end do
-    !end function dpdz
 
     subroutine dpdz(z, p, prhs)
         import :: ne, zex, f, ic, dtr
         implicit none
 
-        real(c_double), intent(in) :: z, p(:)
-        real(c_double), intent(out) :: prhs(:)
+        real(c_double) z, p(*), prhs(*)
 
         integer(c_int) i, reidx(ne), imidx(ne)
         real(c_double) u
@@ -305,29 +258,26 @@ contains
         xi = (0.5d0*(mean(1) + mean(2)) + sum(mean(2:nz - 1)))*dz
     end function
 
-    function dfdt(t, f) result(s)
+    subroutine dfdt(neqf, t, f, s, rpar, ipar)
         implicit none
 
-        integer(c_int) :: ii, jj, iter_num = 1, time_num = 1
-        real(c_double) t, f(:), s(size(f)), &
+        integer(c_int) :: ii, jj, iter_num = 1, time_num = 1, neqf, ipar
+        real(c_double) t, f(neqf), s(neqf), &
             x1r, x1i, q31, i1, r1, th1, dcir1, cos1, sin1, &
             x2r, x2i, q32, i2, r2, th2, dcir2, cos2, sin2, q3, &
-            f1, f2, f3, phi1, phi2, phi3, a1, a2
+            f1, f2, f3, phi1, phi2, phi3, a1, a2, zwant, zgot, rpar
         complex(c_double_complex) x1, x2
+
+        call d02pvf(neqp, zstart, p(:, 1), zex, ptol, thres, method, 'usual task', errass, hstart, workp, lenwrk, ifail)
 
         fp(1) = f(1)*exp(ic*f(2))
         fp(2) = f(3)*exp(ic*f(4))
 
-        call ode4p(dpdz, p, ne, nz, 0.0d0, dz)
-
-        !open (1, file='test.dat')
-        !do jj = 1, nz
-        !    do ii = 1, 2*ne
-        !        write (1, '(2f17.8)') dreal(p(ii, jj)), dimag(p(ii, jj))
-        !    end do
-        !end do
-        !close (1)
-        !stop
+        do jj = 1, nz - 1
+            zwant = zax(jj + 1)
+            call d02pcf(dpdz, zwant, zgot, pgot, ppgot, pmax, workp, ifail)
+            p(:, jj + 1) = pgot
+        end do
 
         x1 = xi(p(1:2*ne, :), 1)
         x2 = xi(p(2*ne + 1:4*ne, :), 1)
@@ -380,7 +330,7 @@ contains
             time_num = time_num + 1
         end if
         iter_num = iter_num + 1
-    end function dfdt
+    end subroutine dfdt
 
     subroutine calc_u(u, zex, nz, zax)
         import
@@ -397,5 +347,61 @@ contains
         end do
 
     end subroutine
+
+    subroutine solout(nr, xold, x, y, n, con, icomp, nd, rpar, ipar, irtrn)
+        implicit none
+
+        interface
+            function contd5(ii, x, con, icomp, nd)
+                implicit double precision(a - h, o - z)
+                dimension con(5*nd), icomp(nd)
+            end
+        end interface
+
+        integer(c_int) nr, n, nd, icomp(nd), ipar, irtrn, j
+        real(c_double) xold, x, con(5*nd), rpar, y(neqf), xout
+        logical(4) pressed
+        character(1) key
+        integer(c_int), parameter :: esc = 27
+        common/intern/xout
+
+        if (nr .eq. 1) then
+            write (*, '(a,f12.7,a,f10.7,a,f10.7,a,f10.7,a,f10.7,a,f10.7,\,a)') 'Time = ', xout, '   |F1| = ', abs(y(1)), '   |F2| = ', abs(y(3)), '   |F3| = ', abs(y(5)), &
+                '   Eff1 = ', eta(1, nr), '   Eff2 = ', eta(2, nr), char(13)
+            xout = x + dt
+            eta(:, nr) = eff(p(:, nz))
+            etag(:, nr) = pitch**2/(pitch**2 + 1)*eta(:, nr)
+            do j = 1,neqf
+                f(j,nr) = y(j)
+            enddo
+        else
+10          continue
+            if (x .ge. xout) then
+                write (*, '(a,f12.7,a,f10.7,a,f10.7,a,f10.7,a,f10.7,a,f10.7,\,a)') 'Time = ', xout, '   |F1| = ', abs(y(1)), '   |F2| = ', abs(y(3)), '   |F3| = ', abs(y(5)), &
+                    '   Eff1 = ', eta(1, nr), '   Eff2 = ', eta(2, nr), char(13)
+                xout = xout + dt
+                eta(:, nr) = eff(p(:, nz))
+                etag(:, nr) = pitch**2/(pitch**2 + 1)*eta(:, nr)
+                do j = 1,neqf
+                    f(j,nr) = y(j)
+                enddo
+                goto 10
+            end if
+        end if
+
+        pressed = peekcharqq()
+        if (pressed) then
+            key = getcharqq()
+            if (ichar(key) .eq. esc) then
+                write (*, '(/,a)') 'Quit?'
+                key = getcharqq()
+                if (ichar(key) .eq. 121 .or. ichar(key) .eq. 89) then
+                    nt = nr
+                    return
+                end if
+            end if
+        end if
+        return
+    end
 
 end module fun
